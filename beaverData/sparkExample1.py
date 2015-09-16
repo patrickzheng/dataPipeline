4ceb3461ce12070c29c24abaee08199b595fd736
patrick <patrick.li.zheng@gmail.com>
2015-09-16 07:02:35 +0000

    first spark program

--- beaverData/sparkExample1.py
+++ beaverData/sparkExample1.py
@@ -1,12 +1,17 @@
 from pyspark import SparkContext 
-# sc = SparkContext(appName="TestApp")
+sc = SparkContext(appName="TestApp")
 
 from pyspark.sql import SQLContext
 sqlContext = SQLContext(sc)
 
-#text_file = spark.textFile("s3://amazon-reviews/complete.json")
-df = sqlContext.read.json("s3://patricks3db/meta_Movies_and_TV.json")
-counts = text_file.flatMap(lambda line: line.split(" ")) \
-             .map(lambda word: (word, 1)) \
-             .reduceByKey(lambda a, b: a + b)
-counts.saveAsTextFile("/beaverData/jobTemp")
+# text_file = sc.textFile("s3://amazon-reviews/complete.json")
+text_file = sc.textFile(    "s3://patricks3db/meta_Movies_and_TV.json")
+# df = sqlContext.read.json("s3://patricks3db/meta_Movies_and_TV.json")
+
+# connect to cassandra
+from cassandra.cluster import Cluster
+cluster = Cluster(['172.31.39.223', '172.31.39.224','172.31.39.225','172.31.39.226'])
+session = cluster.connect()
+
+insert_statment = session.prepare('INSERT INTO playground.metadata JSON ?')
+counts = text_file.map(lambda line: session.execute(insert_statment, line))
