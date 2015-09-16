8e40101357f9e05ee56b1a55f24c59a4b1f3cdc8
patrick <patrick.li.zheng@gmail.com>
2015-09-16 18:33:23 +0000

    Spark to Json

--- beaverData/sparkExample1.py
+++ beaverData/sparkExample1.py
@@ -5,7 +5,7 @@ from pyspark.sql import SQLContext
 sqlContext = SQLContext(sc)
 
 # text_file = sc.textFile("s3://amazon-reviews/complete.json")
-text_file = sc.textFile(    "s3://patricks3db/meta_Movies_and_TV.json")
+# text_file = sc.textFile(    "s3://patricks3db/meta_Movies_and_TV.json")
 # df = sqlContext.read.json("s3://patricks3db/meta_Movies_and_TV.json")
 
 # connect to cassandra
@@ -14,4 +14,12 @@ cluster = Cluster(['172.31.39.223', '172.31.39.224','172.31.39.225','172.31.39.2
 session = cluster.connect()
 
 insert_statment = session.prepare('INSERT INTO playground.metadata JSON ?')
-counts = text_file.map(lambda line: session.execute(insert_statment, line))
+# counts = text_file.map(lambda line: session.execute(insert_statment, line))
+
+# test another way
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
+bucket = conn.get_bucket('patricks3db')
+key = bucket.get_key("meta_Movies_and_TV.json")
+for line in  key.get_contents_as_string().splitlines():
+	session.execute(insert_statment, line)
